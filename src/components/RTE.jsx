8f8718fbed808656e114a9f90ffa1921e93f51a2
moxitparmar <moxitparmar2004@gmail.com>
2024-08-02 09:15:39 +0530

    fixed .env problem and first stable version

---
+++ src/components/RTE.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+import conf from "../conf/conf.js";
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block pl-1 mb-1">{label}</label>}
+
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            apiKey="1g29d7es48y1h4gq9fi3uh67tz0h1o9jcrtg8yprev8azct9"
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+}
